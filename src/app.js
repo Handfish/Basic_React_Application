7b00ab9559501417f9b08cd7ccdca859e33d51f3
Kenny <kennyudovic@gmail.com>
2017-12-21 10:07:04 -0500

    Saving options to local storage

--- src/app.js
+++ src/app.js
@@ -10,6 +10,27 @@ class IndecisionApp extends React.Component {
     }
   }
 
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if(options)
+        this.setState(() => ({ options }))
+    } 
+    catch (e) {
+       
+    }
+
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    } 
+  }
+
   handlePick() {
     const randomIndex = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomIndex];
@@ -97,6 +118,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started!</p>}
       {
         props.options.map((option) => 
           <Option 
@@ -140,6 +162,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = ''; 
+    }
   }
 
   render() {
